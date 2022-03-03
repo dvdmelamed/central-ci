815f2e440b36a26df45764f6814e29682c8b48bc
dvdmelamed <dvdmelamed@gmail.com>
2022-03-03 18:31:20 +0200

    Update README

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # Centralized CI
 
-Add the following environment variables.
+This repository is a companion to the talk given at Worldwide Software Architect 2022: "FORGET MONO VS. MULTI-REPO: BUILDING CENTRALIZED GIT WORKFLOWS IN PYTHON"
+
+Create a Github application and add the following environment variables to your environment:
 ``GH_SECRET``: The secret key from your GitHub App
 ``GH_APP_ID``: The ID of your GitHub App
 ``GH_PRIVATE_KEY``: The private key of your GitHub App. It looks like:
 
 To allow inbound traffic:
 ```
-ngrok http --region=eu --hostname=centralci.eu.ngrok.io 800
+ngrok http --region=eu --hostname=<domain>.ngrok.io 800
 ```
 
 Run
--- src/main.py
+++ src/main.py
@@ -202,27 +202,6 @@ async def webhook(request):
         return web.Response(status=500)
 
 
-@router.register("installation", action="created")
-async def repo_installation_added(event, gh, *args, **kwargs):
-    installation_id = event.data["installation"]["id"]
-
-    installation_access_token = await apps.get_installation_access_token(
-        gh,
-        installation_id=installation_id,
-        app_id=os.environ.get("GH_APP_ID"),
-        private_key=os.environ.get("GH_PRIVATE_KEY")
-    )
-
-    repo_name = event.data["repositories"][0]["full_name"]
-    url = f"/repos/{repo_name}/issues"
-    response = await gh.post(
-        url,
-        data={'title': 'Thanks for installing my bot', 'body': 'Thanks!'},
-        oauth_token=installation_access_token["token"]
-    )
-    return response
-
-
 @router.register("check_suite", action="requested")
 async def branch_created(event, gh, *args, **kwargs):
     print("Branch created")
@@ -242,17 +221,4 @@ async def pull_request_reopened(event, gh, *args, **kwargs):
 def create_app(loop):
     app = web.Application()
     app.router.add_routes(routes)
-    port = os.environ.get("PORT")
-    if port is not None:
-        port = int(port)
     return app
-
-
-# if __name__ == "__main__":
-#     app = web.Application()
-#
-#     app.router.add_routes(routes)
-#     port = os.environ.get("PORT")
-#     if port is not None:
-#         port = int(port)
-#     web.run_app(app, port=port)
